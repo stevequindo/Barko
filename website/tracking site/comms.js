0e887af9d3d4d53de08dd0eb036f56a38e6a0930
nchong128 <nchong128@gmail.com>
2019-03-08 19:56:41 +1100

    Added prev website for inspo

---
+++ website/tracking site/comms.js
@@ -0,0 +1,7 @@
+var showthingy = document.getElementById("add");
+
+
+showthingy.addEventListener("click",function(){
+    // document.getElementById("ex-1").style.display='block';
+    $("#ex-1").fadeIn();
+})
