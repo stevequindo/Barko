c26912da343c0d4497da602897c39fc035091d03
nchong128 <nchong128@gmail.com>
2019-04-12 12:21:44 +1000

    .gitignore remove demo website

--- website/tracking site/comms.js
+++
@@ -1,7 +0,0 @@
-var showthingy = document.getElementById("add");
-
-
-showthingy.addEventListener("click",function(){
-    // document.getElementById("ex-1").style.display='block';
-    $("#ex-1").fadeIn();
-})
