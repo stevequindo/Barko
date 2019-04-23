e55fa3cd559abb58268cc4c3a2f7a1b97dfac297
nchong128 <nchong128@gmail.com>
2019-04-23 20:12:29 +1000

    Route to error page if no containers exist

--- app.js
+++ app.js
@@ -167,7 +167,21 @@ app.get('/recent', isLoggedIn, async (req,res) => {
 
         res.redirect(`/overview/country/${results.departureCountry}/id/${results._id}`);
     } catch(e) {
-        res.redirect("/");
+        let mHeader, mBody;
+        if (e instanceof TypeError) {
+            // Occurs if nothing is no transactions are found
+            mHeader = "It appears no files have been uploaded yet.";
+            mBody = `Please try again later or contact us <a href='mailto:nchong128@gmail.com' style="color: black">here</a>`;
+        } else {
+            mHeader = "It appears an unknown error has occured.";
+            mBody = `Please contact us <a href='mailto:nchong128@gmail.com'>here</a> giving us the error message ${e}`;
+        }
+
+        res.render("error/message", {
+            user: user,
+            messageHeader: mHeader,
+            messageBody: mBody,
+        });
     }
 });
 
