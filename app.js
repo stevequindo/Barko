f0ab79775891f58e63108639024a1bd4ef79af69
nchong128 <nchong128@gmail.com>
2019-05-04 13:03:34 +1000

    Update senders and receivers to include all info from the excel template

--- app.js
+++ app.js
@@ -59,7 +59,6 @@ app.get('/', function(req,res){
 });
 
 app.post('/login', passport.authenticate('local-login', {
-
     successRedirect : '/profile', // redirect to the secure profile section
     failureRedirect : '/', // redirect back to the signup page if there is an error
     failureFlash : true // allow flash messages
