fa3d2a0eb4cbb39da9980908e3c02f1d64d09ec1
nchong128 <nchong128@gmail.com>
2019-07-23 17:51:02 +0800

    Minor control strructure changes

--- validation/jwttoken.js
+++ validation/jwttoken.js
@@ -5,26 +5,23 @@ const jwt = require("jsonwebtoken");
 module.exports = function getJWTToken(req, res, next) {
 	const header = req.headers['authorization'];
 
-	if (typeof header === 'undefined') {
+	if (typeof header !== 'undefined') {
+		const bearer = header.split(' ');
+		const token = bearer[1];
+
+		//verify the JWT token generated for the user
+		jwt.verify(token, keys.secretOrKey, (err, authorizedData) => {
+			if (err) {
+				res.sendStatus(403);
+			} else {
+				//If token is successfully verified, we can send the authorized data
+				req.authorizedData = authorizedData;
+			}
+		});
+
+	} else {
 		res.sendStatus(403);
 	}
-
-	const bearer = header.split(' ');
-	const token = bearer[1];
-
-	//verify the JWT token generated for the user
-	jwt.verify(token, keys.secretOrKey, (err, authorizedData) => {
-		if(err) {
-			//If error send Forbidden (403)
-			console.log('ERROR: Could not connect to the protected route');
-			res.sendStatus(403);
-		} else {
-			//If token is successfully verified, we can send the authorized data
-			console.log('SUCCESS: Connected to protected route');
-			req.authorizedData = authorizedData;
-		}
-	});
-
 
 	next();
 };
