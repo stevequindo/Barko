8d5401068b3f1d646ff6559e32abb817ccbff3f9
nchong128 <nchong128@gmail.com>
2019-07-26 00:23:41 +0800

    Implement different navbar reslts based on if user is logged in or not

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,22 +1,62 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import './NavBar.css';
+import {connect} from "react-redux";
+import {logoutUser} from "../../actions/authActions";
+import PropTypes from "prop-types";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <nav className="blue accent-3">
-        <div className="nav-wrapper">
-          <Link to="/" className="brand-logo">
-            Barko
-          </Link>
-          <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="/">...</a></li>
-          </ul>
-        </div>
-      </nav>
-    );
+class NavBar extends Component {
+
+    constructor(props) {
+        super(props);
+        console.log(props);
+    }
+
+    render() {
+        if (this.props.auth.isAuthenticated) {
+            return (
+              <nav className="blue accent-3">
+                <div className="nav-wrapper">
+                  <Link to="/" className="brand-logo">
+                    Barko
+                  </Link>
+                  <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li><a href="/containers">Manifest Files</a></li>
+                    <li><a href="/tracking">Tracking</a></li>
+                    <li><a href="/">Logout</a></li>
+                  </ul>
+                </div>
+              </nav>
+            );
+        } else {
+            return (
+                <nav className="blue accent-3">
+                    <div className="nav-wrapper">
+                    <Link to="/" className="brand-logo">
+                        Barko
+                    </Link>
+                    <ul id="nav-mobile" className="right hide-on-med-and-down">
+                        <li><a href="/tracking">Tracking</a></li>
+                    </ul>
+                    </div>
+                </nav>
+            )
+        }
+
   }
 }
 
-export default Navbar;
+NavBar.propTypes = {
+    logoutUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    auth: state.auth
+});
+
+export default connect(
+    mapStateToProps,
+    { logoutUser }
+)(NavBar);
+
